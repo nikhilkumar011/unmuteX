0275aa5ab3cceea1e72cb4af44601623b32be77d
Nikhil Kumar <nikhilkumar.0112358@gmail.com>
2026-06-21 13:55:09 +0530

    added google verification

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ export const metadata = {
   title: "UnmuteX | Break Hesitation, Build Confidence",
   description: "UnmuteX is the ultimate youth public speaking and confidence-building platform designed to help you speak with clarity, conviction, and zero fear.",
    metadataBase: new URL("https://unmute-x-k3z1.vercel.app/"),
+   verification:{
+    google:"eWNku-j_IAj1SU3jCdSrlX0D1QYV0Tn4dILSvOLPeZ4"
+   }
 };
 
 export default function RootLayout({ children }) {
